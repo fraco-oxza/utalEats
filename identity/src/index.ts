4456a967ee1c1d0a44ad71b31b7c9fac9c8fd06a
Francisco Carvajal Ossa <fraco_oxza@outlook.com>
2024-11-23 23:35:50 -0300

    feat(identity): Implement account login endpoint
    This commit implements the `/account/login` endpoint for the identity
    service. This endpoint allows users to log in using their email and
    password.  Password verification is performed using Argon2.  If the
    credentials are valid, the user's account ID is returned; otherwise, an
    empty object is returned.  Drizzle-ORM is used to query the database.

--- identity/src/index.ts
+++ identity/src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import { db, runMigration } from "./db/connection";
 import argon2 from "argon2";
 import { accountTable } from "./db/schema";
+import { and, eq } from "drizzle-orm";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -36,6 +37,29 @@ app.post("/account/register", async (req, res) => {
         res.status(500).send("Internal server error");
       }
     });
+});
+
+app.post("/account/login", async (req, res) => {
+  const body: {
+    email: string;
+    password: string;
+  } = req.body;
+
+  const account = await db
+    .select()
+    .from(accountTable)
+    .where(eq(accountTable.email, body.email));
+
+  if (account.length === 0) {
+    res.send({});
+  }
+
+  if (await argon2.verify(account[0].passwordHash, body.password)) {
+    res.send({ accountId: account[0].id });
+    return;
+  }
+
+  res.send({});
 });
 
 app.listen(port, () => {
