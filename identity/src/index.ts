d9572e5ff76e089f4384b755b163aac7546d29fe
Francisco Carvajal Ossa <fraco_oxza@outlook.com>
2024-11-23 23:26:36 -0300

    feat(identity): Implement account registration endpoint
    This commit implements the `/account/register` endpoint for the identity
    service.  This endpoint allows users to register new accounts.  Password
    hashing using Argon2 is implemented to securely store passwords.  Error
    handling is included to manage duplicate email addresses and other
    potential errors. The necessary `argon2` package has been added to
    `package.json`.  The server now also parses JSON requests.

--- identity/src/index.ts
+++ identity/src/index.ts
@@ -1,10 +1,42 @@
 import express, { Express } from "express";
 import { db, runMigration } from "./db/connection";
+import argon2 from "argon2";
+import { accountTable } from "./db/schema";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 runMigration();
+
+app.post("/account/register", async (req, res) => {
+  const body: {
+    email: string;
+    password: string;
+    name: string;
+    phone: string;
+  } = req.body;
+
+  const newAccount: typeof accountTable.$inferInsert = {
+    ...body,
+    passwordHash: await argon2.hash(body.password),
+  };
+
+  db.insert(accountTable)
+    .values(newAccount)
+    .then(() => res.send(newAccount))
+    .catch((e) => {
+      if (
+        e.message.includes("duplicate key value violates unique constraint")
+      ) {
+        res.status(400).send("Email already exists");
+      } else {
+        console.error(e);
+        res.status(500).send("Internal server error");
+      }
+    });
+});
 
 app.listen(port, () => {
   console.info(`Server running on port ${port}`);
