14c31703cb3c1ec48d57a4a6382d05987b4fbd7b
Francisco Carvajal Ossa <fraco_oxza@outlook.com>
2024-11-27 15:06:45 -0300

    feat(stores): Implement stores service with seed data
    This commit implements the basic functionality for the `stores` service.
    It includes endpoints to retrieve stores by city name and products by
    store ID.  The `src/index.ts` file now connects to the MongoDB database
    and seeds the database with initial store and product data from
    `src/initialStores.json`.  Error handling is included for missing
    `storeId` query parameters and for stores not found.

--- stores/src/index.ts
+++ stores/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import { MongoClient } from "mongodb";
 import morgan from "morgan";
+import { readFile } from "fs/promises";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -13,11 +14,29 @@ if (!uri) {
 
 const client = new MongoClient(uri);
 
+async function seedDatabase() {
+  // Check if the database is already seeded
+  const db = client.db("store");
+  const collection = db.collection("stores");
+  const stores = await collection.find({}).toArray();
+  if (stores.length > 0) {
+    return;
+  }
+
+  const newStores = JSON.parse(
+    await readFile("src/initialStores.json", "utf-8")
+  );
+  console.log(newStores);
+
+  await collection.insertMany(newStores);
+}
+
 // Check connection
 client
   .connect()
   .then(() => {
     console.info("Database connected");
+    seedDatabase();
   })
   .catch((err) => {
     console.error(err);
@@ -26,6 +45,52 @@ client
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.get("/store", async (req, res) => {
+  const cityName = req.query.cityName;
+
+  const db = client.db("store");
+  const collection = db.collection("stores");
+
+  const query = cityName ? { cityName } : {}; // If cityName is provided, filter by cityName, else return all
+  const options = {};
+
+  const stores = await collection.find(query, options).toArray();
+
+  const response = stores.map((store) => ({
+    storeId: store.storeId,
+    storeName: store.storeName,
+  }));
+
+  res.json(response);
+});
+
+app.get("/product", async (req, res) => {
+  const storeIdRaw = req.query.storeId as string;
+
+  if (storeIdRaw === undefined) {
+    res.status(400).send("storeId is required");
+    return;
+  }
+
+  const storeId = parseInt(storeIdRaw);
+  console.log(storeId);
+
+  const db = client.db("store");
+  const collection = db.collection("stores");
+
+  const query = { storeId };
+  const options = {};
+
+  const store = await collection.findOne(query, options);
+
+  if (!store) {
+    res.status(404).send("Store not found");
+    return;
+  }
+
+  res.json(store.products);
+});
 
 app.listen(port, () => {
   console.info(`Server running on port ${port}`);
