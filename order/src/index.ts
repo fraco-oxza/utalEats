f7a374662ec287846aa79b007cee6a37b2fbf2fb
Francisco Carvajal Ossa <fraco_oxza@outlook.com>
2024-12-03 12:57:54 -0300

    feat(order): Implement order service
    This commit introduces the order service, responsible for managing orders.
    
    The service includes:
    
    - API endpoints for creating and retrieving orders.
    - Data validation using Zod.
    - Persistence using MongoDB.
    - Logging using Morgan.
    - A Dockerfile for containerization.
    - Configuration files for development (nodemon.json, tsconfig.json).

---
+++ order/src/index.ts
@@ -0,0 +1,88 @@
+import express, { Express } from "express";
+import { MongoClient } from "mongodb";
+import morgan from "morgan";
+import { z } from "zod";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+const uri = process.env.DATABASE_URL;
+
+const OrderSchema = z.object({
+  accountId: z.string(),
+  storeId: z.string(),
+  products: z.array(
+    z.object({
+      product: z.string(),
+      quantity: z.number(),
+    })
+  ),
+});
+
+if (!uri) {
+  throw new Error("Database URL is not defined");
+}
+
+const client = new MongoClient(uri);
+
+// Check connection
+client
+  .connect()
+  .then(() => {
+    console.info("Database connected");
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.post("/", (req, res) => {
+  let payload: typeof OrderSchema._type;
+
+  try {
+    payload = OrderSchema.parse(req.body);
+  } catch (err) {
+    // Response what is the error with the data
+    res.status(400).send(err);
+
+    return;
+  }
+
+  const db = client.db("order");
+  const collection = db.collection("orders");
+
+  collection
+    .insertOne(payload)
+    .then(() => {
+      res.status(201).send("Order created");
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Internal server error");
+    });
+});
+
+app.get("/", (req, res) => {
+  const accountId = req.query.accountId as string;
+
+  const db = client.db("order");
+  const collection = db.collection("orders");
+
+  collection
+    .find({ accountId })
+    .toArray()
+    .then((orders) => {
+      res.status(200).send(orders);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Internal server error");
+    });
+});
+
+app.listen(port, () => {
+  console.info(`Server running on port ${port}`);
+});
