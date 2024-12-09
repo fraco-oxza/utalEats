56f94a45afbca24588e2ed7c52b46e11b7b8dcbb
Benjamín Farías <108445832+TheAssassin13@users.noreply.github.com>
2024-12-09 19:46:37 -0300

    feat: add logic to cart page (#45)

---
+++ web-app/src/components/CartContainer.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import ItemCart from './ItemCart';
+import "./CartContainer.css";
+
+const CartContainer = () => {
+  const [savedItems, setSavedItems] = useState([]);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('savedItems')) || [];
+    setSavedItems(items);
+  }, []);
+
+  const totalPrice = savedItems.reduce((sum, item) => {
+    return sum + item.price * item.quantity;
+    }, 0);
+
+  return (
+    <div className="container">
+        <ul id="cart-container">
+            {
+                savedItems.map((item, index) => (
+                <ItemCart
+                    key={index}
+                    name={item.name}
+                    imagePath={item.imagePath}
+                    price={item.price}
+                    quantity={item.quantity}
+                />
+                ))
+            }
+        </ul>
+        <div>
+            <p>Total ${totalPrice}</p>
+        </div>
+    </div>
+  );
+};
+
+export default CartContainer;
