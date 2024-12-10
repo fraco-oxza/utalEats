691aba41930686047803db6a17b3486cc0f450a0
Benjamín Farías <108445832+TheAssassin13@users.noreply.github.com>
2024-12-10 10:35:33 -0300

    fix: solve web app bugs (#56)
    * fix: solve visual errors
    
    * fix: solve minor bugs

--- web-app/src/components/ItemCart.jsx
+++ web-app/src/components/ItemCart.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "./ItemCart.css";
 
 
-const ItemCart = ({name, imagePath, price, quantity, isOrderDetail}) => {
-    const savedItems = JSON.parse(
-        localStorage.getItem("savedItems") || "[]",
-    );
+const ItemCart = ({name, imagePath, price, quantity, isOrderDetail, increaseTotalPrice}) => {
+    const [savedItems, setSavedItems] = useState([]);
+    const [quantityDyn, setQuantityDyn] = useState(quantity);
+
+    useEffect(() => {
+        setSavedItems(JSON.parse(
+            localStorage.getItem("savedItems") || "[]",
+        ));
+    }, []);
 
     const addItemToCart = () => {
             const existingItem = savedItems.find((item) => item.name === name);
             existingItem.quantity += 1;
+            increaseTotalPrice(existingItem.price);
+            setQuantityDyn(existingItem.quantity);
+            setSavedItems(savedItems);
             localStorage.setItem("savedItems", JSON.stringify(savedItems));
-            window.location.reload();
     };
 
     const removeItemFromCart = () => {
@@ -19,11 +26,15 @@ const ItemCart = ({name, imagePath, price, quantity, isOrderDetail}) => {
             if (existingItem.quantity <= 1) {
                 const updatedItems = savedItems.filter(item => item.name !== name);
                 localStorage.setItem('savedItems', JSON.stringify(updatedItems));
+                window.location.reload();
             } else {
                 existingItem.quantity -= 1;
+                increaseTotalPrice(-existingItem.price);
+                setQuantityDyn(existingItem.quantity);
+                setSavedItems(savedItems);
                 localStorage.setItem("savedItems", JSON.stringify(savedItems));
             }
-            window.location.reload();
+            
     };
 
     return (
@@ -33,7 +44,7 @@ const ItemCart = ({name, imagePath, price, quantity, isOrderDetail}) => {
                 <div className="item">
                     <p>{name}</p>
                     <div className="price-div">
-                        <p>CLP</p>
+                        <p>USD</p>
                         <p className="price">${price}</p>
                     </div>
                 </div>
@@ -45,7 +56,7 @@ const ItemCart = ({name, imagePath, price, quantity, isOrderDetail}) => {
                 stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round" 
                 className="change-item">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /></svg>
-                <p>{quantity}</p>
+                <p>{quantityDyn}</p>
                 <svg role="button" onClick={addItemToCart} xmlns="http://www.w3.org/2000/svg"  width="24"  height="24" 
                 viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2" 
                 stroke-linecap="round"  stroke-linejoin="round" 
